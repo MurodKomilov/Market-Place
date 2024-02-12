166a1558395bf7946243caa396c4910908c46e34
mkkom <mk19komilov@gmail.com>
2024-02-12 18:18:41 +0500

    Fixed the the problem with Validation and Button enabled

--- app/src/main/java/com/example/marketplace/MainActivity.kt
+++ app/src/main/java/com/example/marketplace/MainActivity.kt
@@ -8,6 +8,7 @@ import android.graphics.PorterDuff
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.MotionEvent
 import android.widget.EditText
 import android.widget.Toast
@@ -18,83 +19,90 @@ import com.example.marketplace.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.logIn.isEnabled = false
+        binding.name.addTextChangedListener(textWatcher)
+        binding.lastName.addTextChangedListener(textWatcher)
 
-        setUpEditText(binding.name)
-        setUpEditText(binding.lastName)
 
-        binding.logIn.setOnClickListener {
-            Toast.makeText(this, "Bosildi", Toast.LENGTH_SHORT).show()
-//            startActivity(Intent(this,MenuActivity::class.java))
+    }
+
+    private val textWatcher = object : TextWatcher {
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
         }
-    }
 
-    private fun setUpEditText(editText: EditText) {
-        editText.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0)
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            binding.name.background.clearColorFilter()
+            binding.lastName.background.clearColorFilter()
 
-        editText.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+            icClear(binding.name)
+            icClear(binding.lastName)
 
-            @SuppressLint("ClickableViewAccessibility")
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                editText.background.clearColorFilter()
+        }
 
-                editText.setOnTouchListener { _, event ->          // Логика иконки крестика
-                    if (event.action == MotionEvent.ACTION_UP &&
-                        editText.compoundDrawablesRelative.getOrNull(2) != null) {
+        override fun afterTextChanged(s: Editable?) {
 
-                        val clearButtonPosition = editText.width - editText.paddingEnd
-                        if (event.rawX >= (clearButtonPosition - editText.compoundDrawablesRelative[2].bounds.width())) {
-                            editText.setText("")
-                            return@setOnTouchListener true
-                        }
-                    }
-                    false
-                }
-            }
+            cyrillicValidation(binding.lastName)  //Проверка Валидацию на Кириллице
+            cyrillicValidation(binding.name)      //Проверка Валидацию на Кириллице
 
-            override fun afterTextChanged(s: Editable?) {       // Проверка на Валидацию, если не правильно то просвечивается красным
-                val text = s.toString()
 
-                if (text.contains(Regex("[^А-Яа-я]"))) {
-                    editText.background.setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP)
-                } else {
-                    editText.background.clearColorFilter()
-                }
+            val text1 = binding.name.text.toString()
+            val text2 = binding.lastName.text.toString()
 
-                editText.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                    0, 0, if (s.isNullOrEmpty()) 0 else R.drawable.clear_btn, 0
-                )
+            val isNameCyrillic = isTextCyrillic(text1)
+            val isLastNameCyrillic = isTextCyrillic(text2)
 
-                checkEditTexts()
-            }
-        })
-    }
-
-    @SuppressLint("ResourceAsColor")
-    private fun checkEditTexts() {                               // Влючение кнрпки Log In
-        val nameFilled = binding.name.text.isNotEmpty()
-        val lastNameFilled = binding.lastName.text.isNotEmpty()
-
-        var nameValidate = binding.name.text.contains(Regex("[^А-Яа-я]"))
-        var lastNameValidate = binding.lastName.text.contains(Regex("[^А-Яа-я]"))
-
-        if (nameFilled && lastNameFilled){
-            if (!nameValidate && !lastNameValidate){
+            if (isNameCyrillic && isLastNameCyrillic){     // Активация кнопки LogIn
                 binding.logIn.background.setColorFilter(0xFFD62F89.toInt(), PorterDuff.Mode.SRC_ATOP)
                 binding.logIn.isEnabled = true
+            } else{
+                binding.logIn.background.setColorFilter(0xFFFF8AC9.toInt(), PorterDuff.Mode.SRC_ATOP)
+                binding.logIn.isEnabled = false
             }
-        } else {
-            binding.logIn.background.setColorFilter(0xFFFF8AC9.toInt(), PorterDuff.Mode.SRC_ATOP)
-            binding.logIn.isEnabled = false
-        }
 
+        }
+    }
+
+    private fun isTextCyrillic(text: String): Boolean {
+        val cyrillicRegex = Regex("[А-Яа-яЁё]+")
+        return cyrillicRegex.matches(text)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private fun icClear(editText:EditText) {
+        Log.d("bosildi", "icClear : $editText ")
+        editText.setOnTouchListener { _, event ->          // Логика иконки крестика
+            if (event.action == MotionEvent.ACTION_UP &&
+                editText.compoundDrawablesRelative.getOrNull(2) != null
+            ) {
+
+                val clearButtonPosition = editText.width - editText.paddingEnd
+                if (event.rawX >= (clearButtonPosition - editText.compoundDrawablesRelative[2].bounds.width())) {
+                    editText.setText("")
+                    return@setOnTouchListener true
+                }
+            }
+           false
+        }
+    }
+
+    fun cyrillicValidation(editText:EditText){     // Валидация на Кириллицу, Если в EditText печатается на крилице, то все хорошо
+        val text = editText.text.toString()         // если нет то EditText покраснеет
+
+        if (text.contains(Regex("[^А-Яа-я]"))) {
+            editText.background.setColorFilter(Color.RED, PorterDuff.Mode.SRC_ATOP)
+        } else {
+            editText.background.clearColorFilter()
+        }
+        editText.setCompoundDrawablesRelativeWithIntrinsicBounds(
+            0, 0, if (text.isNullOrEmpty()) 0 else R.drawable.clear_btn, 0
+        )
 
     }
+
 }
+
+
