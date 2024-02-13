e7224edfcb5d3e9ae694db11bf4727d525b79172
mkkom <mk19komilov@gmail.com>
2024-02-13 18:34:33 +0500

    Fixed problem with bottom navigation menu Add Fragmenst. Needs to make logic

--- app/src/main/java/com/example/marketplace/MenuActivity.kt
+++ app/src/main/java/com/example/marketplace/MenuActivity.kt
@@ -2,10 +2,17 @@ package com.example.marketplace
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.marketplace.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavMenu.setupWithNavController(findNavController(R.id.fragment))
     }
 }
